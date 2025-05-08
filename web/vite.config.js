47bdcf2d3eec05d9c9170d5272b95512f2b368e2
Yury Kuznetsov <ykuznetsov.dev@gmail.com>
2025-05-07 18:30:41 -0700

    Dev and prod mode separation in vite config

--- web/vite.config.js
+++ web/vite.config.js
@@ -2,14 +2,23 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: true,
-    port: 3000,
-    watch: {
-      usePolling: true,
-    },
-  },
+export default defineConfig(({ command, mode }) => {
+  console.log('Command:', command); // 'serve' for dev, 'build' for build
+  console.log('Mode:', mode);
+  if (mode === 'development') {
+    return {
+      plugins: [react()],
+      server: {
+        host: true,
+        port: 3000,
+        watch: {
+          usePolling: true,
+        },
+      },
+    }
+  } else {
+    return {
+      plugins: [react()],
+    }
+  }
 })
-
